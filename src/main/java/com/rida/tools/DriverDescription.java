22c47e4b14cf1bdb427731f1f6bf4b4c7eb9bc37
Unrealwork <unrealwork@gmail.com>
2016-04-04 09:57:25 +0300

    add some comment

--- src/main/java/com/rida/tools/DriverDescription.java
+++ src/main/java/com/rida/tools/DriverDescription.java
@@ -3,6 +3,7 @@ package com.rida.tools;
 import jade.core.AID;
 
 /**
+ * Описывает водителя
  * Created by daine on 04.04.2016.
  */
 public class DriverDescription implements Comparable {
@@ -18,14 +19,21 @@ public class DriverDescription implements Comparable {
         return value;
     }
 
-    private AID value;
-    private int from, to;
-    private int wayLength;
+    private AID value;//уникальный идентификатор
+    private int from, to;// Откуда едем и куда
+    private int wayLength;//Длина пути
     private int profit;
     private int reverseProfit;
-    private Graph mapGraph;
+    private Graph mapGraph;// ссылка на граф
 
-
+    /**
+     * Конструктор класса описания
+     *
+     * @param way  - часть соообщения, содержащая информацию о путе
+     * @param name - Имя агента
+     * @param a    - Уникальный идентификатор агента
+     * @param g    - Граф путей
+     */
     public DriverDescription(String way, String name, AID a, Graph g) {
         this.way = way;
         this.name = name;
@@ -51,10 +59,20 @@ public class DriverDescription implements Comparable {
         return (name.toString() + " " + way);
     }
 
+
+    /**
+     * Расчитать длину пути
+     */
     public void calcWayLength() {
         wayLength = mapGraph.bfs(Integer.parseInt(way.split(" ")[0]), Integer.parseInt(way.split(" ")[1]));
     }
 
+
+    /**
+     * Расчитать выгоду при поездке с данным водителем
+     * @param from
+     * @param to
+     */
     public void calcProfit(int from, int to) {
         int myFrom = Integer.parseInt(way.split(" ")[0]);
         int myTo = Integer.parseInt(way.split(" ")[1]);
@@ -66,7 +84,12 @@ public class DriverDescription implements Comparable {
                 (mapGraph.bfs(myFrom, from) + mapGraph.bfs(from, to) + mapGraph.bfs(to, myTo));
     }
 
-
+    /**
+     * Перегружаем опертаор сравнения
+     * Эквивалентны при равенстве именн и описанию путей
+     * @param another - ссылка на другой объект
+     * @return - эквиваленты объекты или нет
+     */
     @Override
     public boolean equals(Object another) {
         if (another.getClass() != this.getClass()) {
@@ -78,10 +101,12 @@ public class DriverDescription implements Comparable {
 
     }
 
+
     @Override
     public int hashCode() {
         return this.name.hashCode() * this.way.hashCode();
     }
+
 
     public int getReverseProfit() {
         return reverseProfit;
@@ -97,6 +122,11 @@ public class DriverDescription implements Comparable {
             return -1;
         }
     }
+
+    /**
+     * Геттеры и сеттеры для приввтных полей класса
+     * @return
+     */
 
     public String getname() {
         return name;
